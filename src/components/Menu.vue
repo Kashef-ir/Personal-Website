9e05e86500ee1b87c0886ca24a1a12df756506d5
Mohamad Hashemi <muhamadhashemi142@gmail.com>
2025-08-21 12:44:07 +0330

    formatted

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="2xl:mx-80 xl:mx-80 lg:mx-24 md:mx-24 sm:mx-10 mx-12 text-gray-300">
+  <div
+    class="2xl:mx-80 xl:mx-80 lg:mx-24 md:mx-24 sm:mx-10 mx-12 text-gray-300"
+  >
     <ul
       class="flex justify-start items-center font-bold text-gray-400 transition-all ease-in-out text-2xl space-x-6"
     >
