42d5312bcac997eb977df60686e95828511353cf
Mohamad Hashemi <muhamadhashemi142@gmail.com>
2025-08-21 15:37:49 +0330

    refactor contentful.js

--- src/contentful.js
+++ src/contentful.js
@@ -1,5 +1,12 @@
 import * as contentful from "contentful";
 
+console.log("Space ID:", import.meta.env.VITE_CONTENTFUL_SPACE_ID);
+console.log("Access Token:", import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN);
+
+if (!import.meta.env.VITE_CONTENTFUL_SPACE_ID || !import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN) {
+  console.error("⚠️ Contentful environment variables are missing!");
+}
+
 export const client = contentful.createClient({
   space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
   accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
